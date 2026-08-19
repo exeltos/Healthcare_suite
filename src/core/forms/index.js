3faed9438d7dcda7b082686ff85b7cfa5cb9079a
Exeltos <info@exeltos.com>
2026-08-19 12:41:09 +0300

    new9

--- src/core/forms/index.js
+++
@@ -1,4 +0,0 @@
-export { default as useCoreForm } from './useCoreForm'
-export { compose, email, isBlank, maxLength, minLength, numberRange, pattern, required, validateValues } from './validation'
-
-export { buildFieldValidationSchema, buildQuestionValidationSchema } from './schema'
--- src/i18n/index.js
+++
@@ -1,2 +0,0 @@
-export { I18nProvider, useI18n } from './I18nProvider'
-export { translations } from './translations'
